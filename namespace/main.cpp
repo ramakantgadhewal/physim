161eb6f951eb0c8176ec8f239e4f7e31698cb226
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2022-08-19 13:09:00 +0200

    tester

---
+++ namespace/main.cpp
@@ -0,0 +1,12 @@
+
+// author:          Lorenzo Liuzzo
+// email:           lorenzoliuzzo@outlook.com
+// description:     main for checking whether the physim.h compiles. 
+// last updated:    19/08/2022
+
+#include "physim.h"
+
+int main() {
+
+    return 0; 
+}
